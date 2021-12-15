3ec172d29d0bca759875ce85eeaac4777830fc0b
Wildgoosechaze <nelsonstrickland@gmail.com>
2021-12-14 23:56:10 -0500

    recenter section

---
+++ JS/contactScript.js
@@ -0,0 +1,33 @@
+// Dom Elements
+
+// let instagram = document.getElementById("instagramLogo") ;
+// let icons = document.getElementsByClassName('contactLogo') ;
+// let contactButton = document.getElementById("emailButton")
+
+// Global variables
+
+
+// Global Functions 
+
+// function toggle(ID){
+//     var element = document.getElementById(ID);
+//     if(element.style.display === "block"){
+//         element.style.display = "none";
+//     }else{
+//         element.style.display = "block";
+//     }
+// }
+
+
+// event Handlers
+
+//  contactButton.onclick = toggle
+
+let logo = document.getElementById('navLogo')
+
+
+logo.onclick = 
+
+
+
+
