3b87c1b67ce76c7fe3b1cb012b5bc2a94c4604d7
Wildgoosechaze <nelsonstrickland@gmail.com>
2021-12-17 00:32:08 -0500

    grid made/skills section added

--- JS/script.js
+++ JS/script.js
@@ -1,10 +1,1 @@
-let navName = document.getElementsByClassName("nav-title")
-let proj1 = document.getElementById('BDE')
-
-const bounce = () => {
-    proj1.innerHTML = "we did it!"
-}
-
-navName.onclick = bounce
-proj1.onmousedown= bounce
 
